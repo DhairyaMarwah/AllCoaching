a6091bbc980fd5baccbcbb6e0df885b218d468a3
Dhairya Marwah <85454844+DhairyaMarwah@users.noreply.github.com>
2022-03-28 13:56:38 +0530

    added slider

--- main.js
+++ main.js
@@ -4,3 +4,15 @@ hamburger.addEventListener('click',()=>{
   console.log("hello");
   navwrap.classList.toggle('show');
 });
+
+
+$('.popular-cards').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  infinite: true,
+  autoplay: true, 
+  // arrows: true,
+  prevArrow:"<button type='button' class='slick-prev pull-left '><</button>",
+  nextArrow:"<button type='button' class='slick-next pull-right'>></button>"
+      
+});
