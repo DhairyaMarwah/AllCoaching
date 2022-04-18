e2dfc652744cf8a1f76003f762b33b12ab14e029
DHAIRYA <dhairya@DHAIRYAs-MacBook-Air.local>
2022-04-18 14:38:21 +0530

    search filter

--- blog.js
+++ blog.js
@@ -1,97 +1,156 @@
+// let api = "https://api.allcoaching.in/api/v1/admin/blog/all";
+// const searchBar = document.getElementById('search');
+// function fetchData(page) { 
+//   fetch(`${api}/0/5`)
+//     .then((response) => {
+//       if (!response.ok) {
+//         throw Error("ERROR");
+//       }
+//       return response.json();
+//     })
+//     .then((data) => {
+//       const html = data
+//         .map((index) => {
+//             console.log(index);
+//           let blogid = index.id;
+//           let trimtext = index.blogBody.substring(0, 131) + "...";
+//           return `
+//                 <div class="blog">
+//                 <div class="blog-img" >
+//                 <img src="https://api.allcoaching.in/${index.blogFeatureImage}"   onerror="this.src='./assets/images/cardimg.jpg'"/>
+//                 </div>
+//                 <div class="blog-text">
+//                 <h1> ${index.title} </h1>
+//                 <p> ${trimtext}   </p>
+//                 <a    onClick="idparser(${index.id})">Read More </a>
+//                 </div>
+//                   </div>
+//                 `;
+//         })
+//         .join("");
+//       document.querySelector("#app").insertAdjacentHTML("afterbegin", html);
+//     });
+// }
 
- let api="https://api.allcoaching.in/api/v1/admin/blog/all"
+const searchBar = document.getElementById('search');
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
 
-
-function fetchData(page){
-    // fetch(`${api}/${page}/2`)
-    fetch(`${api}/0/5`)
-    .then((response) => {
-        
-        if (!response.ok) {
-                    throw Error("ERROR");
-                 }
-            return response.json();
-    })
-    .then((data)=>{
-        // console.log(data);
-
-        const html=data.map((index)=>{
-            // console.log(index);
-            let blogid=index.id
-            // console.log(blogid);
-            let trimtext=index.blogBody.substring(0, 131)+"..."
-            // console.log(trimtext);
+    const filteredBlogs = ourblogs.filter((blog) => {
+        return (
+            blog.title.toLowerCase().includes(searchString) 
+        );
+    });
+    displayBlog(filteredBlogs);
+});
+let ourblogs = [];
+const loadBlog = async () => {
+    try {
+        const res = await fetch('https://api.allcoaching.in/api/v1/admin/blog/all/0/5');
+        ourblogs = await res.json();
+        console.log(ourblogs);
+        displayBlog(ourblogs);
+    } catch (err) {
+        console.error(err);
+    }
+};
+loadBlog()
+const displayBlog = (blogs) => {
+    const htmlString = blogs
+        .map((blog) => {
+            console.log(blog);
+            let trimtext = blog.blogBody.substring(0, 131) + "...";
             return `
-                <div class="blog">
-                <div class="blog-img" >
-                <img src="${index.blogFeatureImage}"   onerror="this.src='./assets/images/cardimg.jpg'"/>
+            <div class="blog">
+           <div class="blog-img" >
+            <img src="https://api.allcoaching.in/${blog.blogFeatureImage}"   onerror="this.src='./assets/images/cardimg.jpg'"/>
                 </div>
-                <div class="blog-text">
-                <h1> ${index.title} </h1>
-                <p> ${trimtext}   </p>
-                <a    onClick="idparser(${index.id})">Read More </a>
+          <div class="blog-text">
+                <h1> ${blog.title} </h1>
+              <p> ${trimtext}   </p>
+              <a    onClick="idparser(${blog.id})">Read More </a>
+              </div>
                 </div>
-                  </div>
-                `;
+        `;
         })
         .join('');
-        document.querySelector("#app").insertAdjacentHTML("afterbegin", html);
+        document.querySelector("#app").innerHTML = htmlString;
+};
 
-    })
-} 
-let page=0
-fetchData(page)
-function inc(){
-    // document.getElementById("#app").reload(true);
-    console.log("next");
-    page++; 
-    fetchData(page)
-    console.log(page);
-     }
-     function dec(){
-        // document.getElementById("app1").contentWindow.location.reload(true);
-        console.log("prev");
-        if(page>0){
 
-            page=page-1;
-        } 
-        fetchData(page)
-         }  
- function idparser(id){
-     const nonecards=document.getElementById("blog-page")
-     nonecards.classList.toggle("none")
-    //  document.getElementById("blogopen-page-toggle").classList.toggle("revert-none")
-    document.getElementById("blogopen-page-toggle").classList.remove("remove-blogopen")
-     demo=id;
-     console.log(id); 
-     fetchOpen(id)
-     function fetchOpen(id){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let page = 0;
+// fetchData(page);
+function inc() {
+  console.log("next");
+  page++;
+  fetchData(page);
+  console.log(page);
+}
+function dec() {
+  // document.getElementById("app1").contentWindow.location.reload(true);
+  console.log("prev");
+  if (page > 0) {
+    page = page - 1;
+  }
+  fetchData(page);
+}
+function idparser(id) {
+  const nonecards = document.getElementById("blog-page");
+  nonecards.classList.toggle("none");
+  //  document.getElementById("blogopen-page-toggle").classList.toggle("revert-none")
+  document
+    .getElementById("blogopen-page-toggle")
+    .classList.remove("remove-blogopen");
+  demo = id;
+  console.log(id);
+  fetchOpen(id);
+  function fetchOpen(id) {
     console.log(id);
     fetch(`https://api.allcoaching.in/api/v1/admin/blog/byId/${id}`)
-    .then((response) => {
+      .then((response) => {
         if (!response.ok) {
-                    throw Error("ERROR");
-                 }
-            return response.json();
-    })
-    .then((data)=>{
+          throw Error("ERROR");
+        }
+        return response.json();
+      })
+      .then((data) => {
         console.log(data);
 
-        
-         
-        document.querySelector("#blog-opened").innerHTML = 
-        `<h1>${data.title}</h1>
+        document.querySelector(
+          "#blog-opened"
+        ).innerHTML = `<h1>${data.title}</h1>
         <img src="${data.featureImage}" onerror="this.src='./assets/images/cardimg.jpg'"  />
         <p>${data.blogBody}</p>
-        `
-        ;
+        `;
+      });
+  }
+}
 
-    })
-} 
- } 
-
-
- function closeBlogOpen(){
-     document.getElementById("blogopen-page-toggle").classList.toggle("remove-blogopen")
-    document.getElementById("blog-page").classList.remove("none")
- }
+function closeBlogOpen() {
+  document
+    .getElementById("blogopen-page-toggle")
+    .classList.toggle("remove-blogopen");
+  document.getElementById("blog-page").classList.remove("none");
+}
+// const searchUser = document.querySelector("#search"); 
+// document.getElementById("search").addEventListener("input", () => {
+//   console.log(document.getElementById("search").value);
+// });
